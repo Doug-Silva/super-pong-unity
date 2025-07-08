f1007bf488028d45588e8ea8d304e1699e44c2a1
Doug-Silva <silvatec.br@gmail.com>
2025-07-07 21:23:24 -0300

    update paddle

---
+++ Assets/Scripts/Paddle.cs
@@ -0,0 +1,24 @@
+using UnityEngine;
+
+public class NewMonoBehaviourScript : MonoBehaviour
+{
+    //velocidade
+    public float speed = 5;
+
+    private void Update()
+    {
+        float movement = 0;
+
+        if (Input.GetKey(KeyCode.W))
+        {
+            movement = 1;
+        }
+
+        if (Input.GetKey(KeyCode.S))
+        {
+            movement = -1;
+        }
+
+        transform.Translate(0, movement * speed * Time.deltaTime, 0);
+    }
+}
